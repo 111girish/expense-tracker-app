263550bddc60b95aecf0f070ee0811649c541e5a
111girish <pokhrelgirish94@gmail.com>
2026-02-22 22:40:22 +0545

    Changed urls.py

--- backend_final/expenses/urls.py
+++ backend_final/expenses/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+  path('signup/', views.signup, name='signup'),
+  path('login/', TokenObtainPairView.as_view(), name='login'),
+  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
   path('expenses/', views.expense_list, name='expense_list'),
   path('expenses/<int:pk>', views.expense_detail, name='expense_detail'),
 ]
