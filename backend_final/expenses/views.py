6ca799457d50dec4560996873b3a5e40971c44df
111girish <pokhrelgirish94@gmail.com>
2026-02-22 22:39:22 +0545

    created signup function and allows permission classes

--- backend_final/expenses/views.py
+++ backend_final/expenses/views.py
@@ -4,8 +4,24 @@ from rest_framework.response import Response
 from rest_framework import status
 from .models import Expense
 from .serializers import ExpenseSerializer
+from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def signup(request):
+  serializer = UserSerializer(data=request.data)
+  if serializer.is_valid():
+    serializer.save()
+    return Response({
+      'message': 'User Created Succssfully',
+      'user': serializer.data
+    }, status=status.HTTP_201_CREATED)
+  return Response(serializer.errors, status=status.HTTP_404_BAD_REQUEST)
+
+
+
 
 @api_view(['GET', 'POST'])
 def expense_list(request):
