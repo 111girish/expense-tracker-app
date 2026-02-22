11a01610e97280433464b66bdcc48fa06373b2f1
111girish <pokhrelgirish94@gmail.com>
2026-02-22 22:38:25 +0545

    Changed serializers to not include user

--- backend_final/expenses/serializers.py
+++ backend_final/expenses/serializers.py
@@ -4,4 +4,6 @@ from .models import Expense
 class ExpenseSerializer(serializers.ModelSerializer):
   class Meta: 
     model = Expense
-    fields = '__all__'
+    fields = ['id', 'title', 'amount', 'category', 'date', 'description', 'created_at', 'updated_at']
+    read_only_fields = ['id', 'created_at', 'updated_at']
+    
