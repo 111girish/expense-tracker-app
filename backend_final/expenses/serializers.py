18c6a199c9419b9bea4a87c417a84f60727cc6b5
111girish <pokhrelgirish94@gmail.com>
2026-02-28 22:08:20 +0545

    asdfg

--- backend_final/expenses/serializers.py
+++ backend_final/expenses/serializers.py
@@ -20,7 +20,6 @@ class UserSerializer(serializers.ModelSerializer):
         username=validated_data['username'],
         email=validated_data.get('email', ''),
         password=validated_data['password']
-        )
-      
+        )      
       return user
       
