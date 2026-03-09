39fa65a59897e2ab5f1ebe4aa901c9daeddc350f
111girish <pokhrelgirish94@gmail.com>
2026-03-09 22:57:19 +0545

    Import userSerailizer

--- backend_final/expenses/views.py
+++ backend_final/expenses/views.py
@@ -3,7 +3,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Expense
-from .serializers import ExpenseSerializer
+from .serializers import ExpenseSerializer, UserSerializer
 from rest_framework.permissions import IsAuthenticated, AllowAny
 
 # Create your views here.
